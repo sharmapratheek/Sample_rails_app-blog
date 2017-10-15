1661733ca8ccb1a6362e22d6aecf357dfa3e2a96
Pratheek sharma <sharma.pratheek59@gmail.com>
2017-10-15 19:09:35 +0000

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/pages/new',to: 'pages#new' ,as:'new_page'
   get '/pages/:id/edit',to: 'pages#edit',as:'edit_page'
   get '/pages/:id',to: 'pages#show',as:'page'
-  patch '/pages/:id',to: 'pages#show'
+  patch '/pages/:id',to: 'pages#create'
   delete '/pages/:id',to: 'pages#delete'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
